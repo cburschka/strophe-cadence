a9b87dbcdecb56d0dce2bd635310164f89510d62
Christoph Burschka <christoph@burschka.de>
2016-10-01 13:52:15 +0200

    Fix #1: Add object<->XML serialization.
    storage.write() now encodes data values into a Strophe.Builder object,
    writing XML of the form
    
        <data type="" name=""></data>
    
    Where "type" is one of object, array, boolean, number, string,
    undefined or null, and "name" identifies the key (on a root element,
    or inside a <data type="object"> element)
    
    storage.read() reads the same format from a DOM object.

--- src/storage.js
+++ src/storage.js
@@ -36,10 +36,10 @@ Strophe.addConnectionPlugin('storage', {
    *
    * @param {string} root The element to store.
    * @param {string} namespace The namespace of the element.
-   * @param {DOM|Array} data An array of DOM nodes to insert.
-   * @param {int} timeout The (optional) timeout for the request.
    *
-   * @return {Promise} A promise that resolves to the response stanza.
+   * @return {Builder} A request stanza that can be filled with data.
+   *               The .send(timeout) method will send the stanza to the server,
+   *               and return a promise.
    */
   set(root, namespace) {
     const id = this._c.getUniqueId('storage');
@@ -47,6 +47,63 @@ Strophe.addConnectionPlugin('storage', {
     iq.c('query', {xmlns: Strophe.NS.STORAGE});
     iq.c(root, {xmlns: namespace});
     iq.send = timeout => new Promise((resolve, reject) => this._c.sendIQ(iq, resolve, reject, timeout));
+    iq.write = data => this.write(iq, data);
     return iq;
+  },
+
+  /**
+   * A simple recursive object writer.
+   *
+   * @param {Builder} node A strophe builder.
+   * @param {data} data The data to encode.
+   */
+  write(node, data) {
+    const encodeValue = (val, name) => {
+      node.c('data', {name});
+      if (typeof val !== 'object') {
+        node.attrs({type: typeof val});
+        if (val !== undefined) node.t(String(val));
+      }
+      else if (val instanceof Array) {
+        node.attrs({type: 'array'});
+        val.forEach(encodeValue);
+      }
+      // typeof null == 'object'. Blame Javascript.
+      else if (val === null) node.attrs({type: 'null'});
+      else {
+        node.attrs({type: 'object'});
+        Object.forEach(val,
+          (key, value) => encodeValue(value, key)
+        );
+      }
+      node.up();
+    }
+
+    encodeValue(data);
+    return node;
+  },
+
+  /**
+   * Traverse a DOM object to return a data object.
+   */
+  read(node) {
+    const decodeValue = node => {
+      const value = node.textContent;
+      switch (node.getAttribute('type')) {
+        case 'object': return decodeObject(node);
+        case 'array': return decodeArray(node);
+        case 'number': return parseFloat(value);
+        case 'boolean': return value === 'true';
+        case 'undefined': return undefined;
+        case 'null': return null;
+        default: return value;
+      }
+    };
+    const decodeArray = ({childNodes}) => Array.from(childNodes).map(decodeValue);
+    const decodeObject = ({childNodes}) => Object.fromEntries(Array.from(childNodes).map(
+      e => [e.getAttribute('name'), decodeValue(e)]
+    ));
+
+    return decodeValue(node);
   }
 });
